abe3dda04a7c50ce44bc2b2b4d8fef89910cfb33
Chaim Miller <shira05041056@gmail.com>
2026-09-17 12:33:04 +0300

    add the backend server

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import myAppRoute from "./routes/myApp.route.js"
+import { appError } from "./middleware/errorHendler.js";
+
+
+const app = express()
+const port = process.env.PORT || 3003
+
+app.use(cors({}))
+app.use(express.json())
+
+app.use("/my-app", myAppRoute)
+
+
+app.use((err, req, res, next) => {
+    if (err.statusCode){
+        res.status(err.statusCode).send(err.message);
+    }else{
+        res.status(500).send("somthing wrong");
+    }
+});
+
+
+
+app.listen(port, ()=>{
+    console.log(`server running in http://localhost:${port}`)
+})
